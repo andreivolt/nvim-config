7f8e002cb0272c97f859d8f0f87fce6a472d92c3
Andrei Volt <andrei@avolt.net>
2024-03-28 16:04:16 +0100

    update

--- init.lua
+++ init.lua
@@ -387,19 +387,17 @@ null_ls.setup({
 
 local nvim_lsp = require("lspconfig")
 
--- use an on_attach function to only map the following keys after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
     local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-
     local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
-    -- Enable completion triggered by <c-x><c-o>
     buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
-    -- Mappings.
-    local opts = {noremap = true, silent = true}
+    local opts = {
+        noremap = true,
+        silent = true,
+    }
 
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
     buf_set_keymap("n", "gD", "lua vim.lsp.buf.declaration()", opts)
     buf_set_keymap("n", "gd", "lua vim.lsp.buf.definition()", opts)
     buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
