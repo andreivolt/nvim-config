94e519e6c9779ad1b24cf27ee4939d28001db301
Andrei Volt <andrei@avolt.net>
2025-05-30 00:43:54 +0200

    update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,9 @@
 return {
-  {"pbrisbin/vim-colors-off"},
   {
-    "rafalbromirski/vim-aurora",
+    "projekt0n/github-nvim-theme",
     priority = math.huge,
-    init = function()
-      vim.g.aurora_italic = 1
-      vim.g.aurora_transparent = 1
-      vim.g.aurora_bold = 1
-      vim.g.aurora_darker = 1
+    config = function()
+      vim.cmd("colorscheme github_dark_default")
     end
   },
 }
