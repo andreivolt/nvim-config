1e942f193d46e04ec8048e40a9e478a62c23dbc2
Andrei Volt <andrei@avolt.net>
2025-07-19 19:03:07 +0200

    folding

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -5,44 +5,47 @@ return {
     vim.o.foldlevel = 99
     vim.o.foldlevelstart = 99
   end,
-  config = function()
-    require('ufo').setup({
-      provider_selector = function(bufnr, filetype, buftype)
-        return {'treesitter', 'indent'}
-      end,
-      fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
-        local newVirtText = {}
-        local suffix = (' 󰁂 %d '):format(endLnum - lnum)
-        local sufWidth = vim.fn.strdisplaywidth(suffix)
-        local targetWidth = width - sufWidth
-        local curWidth = 0
-        for _, chunk in ipairs(virtText) do
-          local chunkText = chunk[1]
-          local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-          if targetWidth > curWidth + chunkWidth then
-            table.insert(newVirtText, chunk)
-          else
-            chunkText = truncate(chunkText, targetWidth - curWidth)
-            local hlGroup = chunk[2]
-            table.insert(newVirtText, {chunkText, hlGroup})
-            chunkWidth = vim.fn.strdisplaywidth(chunkText)
-            -- str width returned from truncate() may less than 2nd argument, need padding
-            if curWidth + chunkWidth < targetWidth then
-              suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
-            end
-            break
+  opts = {
+    -- open opening the buffer, close these fold kinds
+    close_fold_kinds_for_ft = {
+      default = { "imports", "comment" },
+      json = { "array" },
+    },
+    provider_selector = function(bufnr, filetype, buftype)
+      return { 'treesitter', 'indent' }
+    end,
+    fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+      local newVirtText = {}
+      local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+      local sufWidth = vim.fn.strdisplaywidth(suffix)
+      local targetWidth = width - sufWidth
+      local curWidth = 0
+      for _, chunk in ipairs(virtText) do
+        local chunkText = chunk[1]
+        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+        if targetWidth > curWidth + chunkWidth then
+          table.insert(newVirtText, chunk)
+        else
+          chunkText = truncate(chunkText, targetWidth - curWidth)
+          local hlGroup = chunk[2]
+          table.insert(newVirtText, { chunkText, hlGroup })
+          chunkWidth = vim.fn.strdisplaywidth(chunkText)
+          -- str width returned from truncate() may less than 2nd argument, need padding
+          if curWidth + chunkWidth < targetWidth then
+            suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
           end
-          curWidth = curWidth + chunkWidth
+          break
         end
-        table.insert(newVirtText, {suffix, 'MoreMsg'})
-        return newVirtText
+        curWidth = curWidth + chunkWidth
       end
-    })
-
-    vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-    vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-
-    vim.keymap.set("n", "<leader>v", require("ufo").peekFoldedLinesUnderCursor)
-  end,
+      table.insert(newVirtText, { suffix, 'MoreMsg' })
+      return newVirtText
+    end
+  },
+  keys = {
+    { 'zR',        function() require('ufo').openAllFolds() end,               desc = 'Open all folds' },
+    { 'zM',        function() require('ufo').closeAllFolds() end,              desc = 'Close all folds' },
+    { '<leader>v', function() require('ufo').peekFoldedLinesUnderCursor() end, desc = 'Peek folded lines' },
+  },
   event = "VeryLazy",
 }
