681e5c269f3b10f4e0419191092dde2023801146
Andrei Volt <andrei@avolt.net>
2025-08-04 09:16:55 +0200

    whitespace

--- lua/plugins/better-whitespace.lua
+++ lua/plugins/better-whitespace.lua
@@ -1,12 +1,5 @@
 return {
   "ntpeters/vim-better-whitespace",
-  init = function()
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      callback = function()
-        vim.cmd("<cmd>StripWhitespaceOnChangedLines<cr>")
-      end,
-    })
-  end,
   event = "VeryLazy",
   lazy = false
 }
