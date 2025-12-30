746da641a6e7792e4f4a1f8bbf44da8ad7c92252
Andrei Volt <andrei@avolt.net>
2025-12-30 19:57:30 +0100

    fix lua_ls workspace detection with explicit root markers

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -14,6 +14,7 @@ return {
     local navic = require("nvim-navic")
 
     require("lspconfig").lua_ls.setup({
+      root_dir = require("lspconfig.util").root_pattern(".git", ".luarc.json", ".luarc.jsonc", "init.lua"),
       on_attach = function(client, bufnr)
           navic.attach(client, bufnr)
       end
