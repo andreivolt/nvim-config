fcb9c136fdededc8a0da7a1495d048e537bb738e
Andrei Volt <andrei@avolt.net>
2025-12-30 19:37:54 +0100

    show all treesitter context lines without limit

--- lua/plugins/treesitter-context.lua
+++ lua/plugins/treesitter-context.lua
@@ -2,7 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter-context",
   dependencies = "nvim-treesitter/nvim-treesitter",
   opts = {
-    max_lines = 3,
+    max_lines = 0,  -- no limit
     mode = "topline"
   },
   event = "VeryLazy"
