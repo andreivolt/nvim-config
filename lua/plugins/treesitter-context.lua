bf6af87e1fb6520e1611aae617287aa543315c05
Andrei Volt <andrei@avolt.net>
2025-12-30 20:34:16 +0100

    skip single-line contexts in treesitter-context (bash parser bug workaround)

--- lua/plugins/treesitter-context.lua
+++ lua/plugins/treesitter-context.lua
@@ -3,7 +3,8 @@ return {
   dependencies = "nvim-treesitter/nvim-treesitter",
   opts = {
     max_lines = 0,  -- no limit
-    mode = "topline"
+    mode = "topline",
+    multiline_threshold = 2,  -- skip single-line contexts (fixes bash one-liner bug)
   },
   event = "VeryLazy"
 }
