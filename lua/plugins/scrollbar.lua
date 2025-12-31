b6c0c4c15602ed7ba5caaedaa3ebff8ef072bfd9
Andrei Volt <andrei@avolt.net>
2025-12-31 22:09:03 +0100

    fix scrollbar hlslens integration syntax

--- lua/plugins/scrollbar.lua
+++ lua/plugins/scrollbar.lua
@@ -1,15 +1,9 @@
 return {
   'petertriho/nvim-scrollbar',
-  opts = {
-    handlers = {
-      cursor = false,
-    },
-  },
-  dependencies = {
-    "kevinhwang91/nvim-hlslens",
-    config = function()
-      require("scrollbar.handlers.search").setup()
-    end,
-  },
+  dependencies = { "kevinhwang91/nvim-hlslens" },
+  config = function()
+    require("scrollbar").setup({ handlers = { cursor = false } })
+    require("scrollbar.handlers.search").setup()
+  end,
   event = "VeryLazy",
 }
