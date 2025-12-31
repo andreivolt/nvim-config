6fd3c2d12e21980c6a6e53740ab5406c36132bc6
Andrei Volt <andrei@avolt.net>
2025-12-31 05:06:41 +0100

    configure Conjure auto-repl and remap K to localleader
    babashka REPL starts automatically when no nREPL found

--- lua/plugins/conjure.lua
+++ lua/plugins/conjure.lua
@@ -6,4 +6,12 @@ return {
     "janet",
     "python",
   },
+  init = function()
+    -- Let LSP handle K (hover) instead of Conjure
+    vim.g["conjure#mapping#doc_word"] = ""
+    -- Auto-start babashka REPL (dies with vim)
+    vim.g["conjure#client#clojure#nrepl#connection#auto_repl#enabled"] = true
+    vim.g["conjure#client#clojure#nrepl#connection#auto_repl#hidden"] = true
+    vim.g["conjure#client#clojure#nrepl#connection#auto_repl#cmd"] = "bb nrepl-server localhost:0"
+  end,
 }
