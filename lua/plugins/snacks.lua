ddae60c494d4dfd9fba31fe49ca0c6648434a02d
Andrei Volt <andrei@avolt.net>
2026-01-01 01:26:10 +0100

    replace telescope with snacks.picker for faster fuzzy finding

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,30 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    picker = {
+      win = {
+        input = {
+          keys = {
+            ["<Esc>"] = { "close", mode = { "i", "n" } },
+            ["<C-u>"] = false,
+          },
+        },
+      },
+      sources = {
+        files = { layout = { preset = "dropdown" } },
+        buffers = { layout = { preset = "dropdown" } },
+        grep = { layout = { preset = "dropdown" } },
+        git_log_file = { layout = { preset = "dropdown" } },
+      },
+    },
+  },
+  keys = {
+    { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
+    { "<leader>ff", function() Snacks.picker.files() end, desc = "Find files" },
+    { "<leader>fg", function() Snacks.picker.grep() end, desc = "Live grep" },
+    { "<leader>fh", function() Snacks.picker.git_log_file() end, desc = "File git history" },
+    { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent files" },
+  },
+}
