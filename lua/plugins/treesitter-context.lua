456d280d1d9f69d48205a70ebd4409638bcb67e0
Andrei Volt <andrei@avolt.net>
2026-01-01 23:28:56 +0100

    remove treesitter-context background, add separator line

--- lua/plugins/treesitter-context.lua
+++ lua/plugins/treesitter-context.lua
@@ -2,8 +2,18 @@ return {
   "nvim-treesitter/nvim-treesitter-context",
   dependencies = "nvim-treesitter/nvim-treesitter",
   opts = {
-    max_lines = 0,  -- no limit
-    mode = "topline"
+    max_lines = 0,
+    mode = "topline",
+    separator = "⎯",
   },
+  config = function(_, opts)
+    require("treesitter-context").setup(opts)
+    local set_hl = function()
+      vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "NONE" })
+      vim.api.nvim_set_hl(0, "TreesitterContextLineNumber", { bg = "NONE" })
+    end
+    set_hl()
+    vim.api.nvim_create_autocmd("ColorScheme", { callback = set_hl })
+  end,
   event = "VeryLazy"
 }
