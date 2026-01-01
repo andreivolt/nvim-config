15a6f09a286e1aa77d2b2e70b1373781e03ad4c9
Andrei Volt <andrei@avolt.net>
2026-01-01 23:11:26 +0100

    add event trigger to treesitter for proper lazy loading

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
   dependencies = {
     { "nvim-treesitter/nvim-treesitter-textobjects" },
   },
+  event = { "BufReadPost", "BufNewFile" },
   cmd = {
     "TSBufDisable",
     "TSBufEnable",
