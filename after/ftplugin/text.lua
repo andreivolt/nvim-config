ee3bc9b7b3153757e1e067bf54a866b595cdfb28
Andrei Volt <andrei@avolt.net>
2026-01-03 15:01:48 +0100

    consolidate spell/text into prose.lua, clean up ftplugins

--- after/ftplugin/text.lua
+++ after/ftplugin/text.lua
@@ -1,23 +1,12 @@
--- maintain aligment after folding TODO
--- local indent_level = fn.indent(vim.v.foldstart)
--- local indent = fn['repeat'](' ', indent_level)
-
 function MyFoldText()
-    local indent = vim.fn.indent(vim.v.foldstart) - vim.o.shiftwidth
-    return string.rep(' ', indent) .. '+' .. string.rep('-', vim.o.shiftwidth - 2) .. ' ' .. string.gsub(vim.fn.getline(vim.v.foldstart), '^%s*', '')
+  local indent = vim.fn.indent(vim.v.foldstart) - vim.o.shiftwidth
+  return string.rep(' ', indent) .. '+' .. string.rep('-', vim.o.shiftwidth - 2) .. ' ' .. string.gsub(vim.fn.getline(vim.v.foldstart), '^%s*', '')
 end
 
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'text',
-  callback = function()
-    vim.wo.foldcolumn = '1'
+vim.bo.expandtab = false
+vim.bo.tabstop = 4
+vim.bo.shiftwidth = 4
 
-    vim.bo.expandtab = false
-    vim.bo.tabstop = 4;
-    vim.bo.shiftwidth = 4
-
-    vim.wo.foldminlines = 0
-    vim.wo.foldmethod = 'indent'
-    vim.wo.foldtext = 'v:lua.MyFoldText()'
-  end
-})
+vim.wo.foldminlines = 0
+vim.wo.foldmethod = 'indent'
+vim.wo.foldtext = 'v:lua.MyFoldText()'
