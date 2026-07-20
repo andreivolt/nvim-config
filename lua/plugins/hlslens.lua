1c5bd941a0aa423cea972532060aa02bc38a4e02
Andrei Volt <andrei@avolt.net>
2026-07-20 17:29:43 +0200

    surface failed search errors as plain messages instead of lua tracebacks in hlslens keymaps

--- lua/plugins/hlslens.lua
+++ lua/plugins/hlslens.lua
@@ -1,16 +1,28 @@
 local kopts = { mode = "n", silent = true }
 
+-- Run a normal-mode search motion; on failure (E486 pattern not found etc.) show the plain error message instead of a lua traceback, and skip starting the lens.
+local function search(cmd)
+  return function()
+    local ok, err = pcall(vim.cmd, cmd)
+    if not ok then
+      vim.notify(err:match("E%d+:.*") or err, vim.log.levels.ERROR)
+      return
+    end
+    require('hlslens').start()
+  end
+end
+
 return {
   "kevinhwang91/nvim-hlslens",
   config = function()
     require('hlslens').setup()
   end,
   keys = {
-    { 'n', function() vim.cmd('execute "normal! " . v:count1 . "n"'); require('hlslens').start() end, kopts },
-    { 'N', function() vim.cmd('execute "normal! " . v:count1 . "N"'); require('hlslens').start() end, kopts },
-    { '*', function() vim.cmd('normal! *'); require('hlslens').start() end, kopts },
-    { '#', function() vim.cmd('normal! #'); require('hlslens').start() end, kopts },
-    { 'g*', function() vim.cmd('normal! g*'); require('hlslens').start() end, kopts },
-    { 'g#', function() vim.cmd('normal! g#'); require('hlslens').start() end, kopts },
+    { 'n', function() search('execute "normal! " . v:count1 . "n"')() end, kopts },
+    { 'N', function() search('execute "normal! " . v:count1 . "N"')() end, kopts },
+    { '*', search('normal! *'), kopts },
+    { '#', search('normal! #'), kopts },
+    { 'g*', search('normal! g*'), kopts },
+    { 'g#', search('normal! g#'), kopts },
   },
 }
